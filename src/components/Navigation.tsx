91a7874f2312841a6c5156edf87b00f18fa40e9f
Alan <alkalinemusic@gmail.com>
2023-10-25 11:06:12 -0400

    fleshing out navbar ideas

---
+++ src/components/Navigation.tsx
@@ -0,0 +1,40 @@
+import React from "react";
+import { Link } from "react-router-dom";
+import "../pages/navbar.css";
+import logo from "../../public/assets/images/720p Black with white background.png";
+import "../pages/logo.css";
+
+function Navbar() {
+  return (
+    <nav className="navbar">
+      <div className="container">
+        <div className="nav-elements">
+          <ul>
+            <img src={logo} alt="Logo" className="navbar-logo" />{" "}
+            {/* Logo image */}
+            <li className="home-link">
+              <Link to="/">Home</Link>
+            </li>
+            <li>
+              <Link to="/music">Music</Link>
+            </li>
+            <li>
+              <Link to="/videos">Videos</Link>
+            </li>
+            <li>
+              <Link to="/streams">Streams</Link>
+            </li>
+            <li>
+              <Link to="/about">About</Link>
+            </li>
+            <li>
+              <Link to="/contact">Contact</Link>
+            </li>
+          </ul>
+        </div>
+      </div>
+    </nav>
+  );
+}
+
+export default Navbar;
