deee5e116f554633269f72feffcec461f462c4e4
Alan <alkalinemusic@gmail.com>
2023-11-08 11:49:13 -0500

    add contact page details and add css and js for popup

---
+++ js/copy-email-text-popup.js
@@ -0,0 +1,10 @@
+// function copyText() {
+//   /* Copy text into clipboard */
+//   navigator.clipboard.writeText("noizcodemusic@gmail.com");
+// }
+
+function myFunction() {
+  var popup = document.getElementById("myPopup");
+  popup.classList.toggle("show");
+  navigator.clipboard.writeText("noizcodemusic@gmail.com");
+}
